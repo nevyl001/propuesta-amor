5867636b0a659c6d59222172e9b10350b5c81a20
Nevyl <christiancastellanosmx@gmail.com>
2025-07-21 14:41:56 -0600

    fotosnew

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState, useEffect, useRef } from "react";
 import confetti from "canvas-confetti";
 import parejaImg from "./assets/pareja.jpg";
 import felicesImg from "./assets/felices.jpg";
-import foto1 from "./assets/foto1.jpg";
-import foto2 from "./assets/foto2.jpg";
-import foto3 from "./assets/foto3.jpg";
-import foto4 from "./assets/foto4.jpg";
-import foto5 from "./assets/foto5.jpg";
+import foto6 from "./assets/foto6.jpg";
+import foto7 from "./assets/foto7.jpg";
+import foto8 from "./assets/foto8.jpg";
+import foto9 from "./assets/foto9.jpg";
+import foto10 from "./assets/foto10.jpg";
 
 import "./App.css";
 
 // ✅ Declaración única de carruselFotos
-const carruselFotos = [foto1, foto2, foto3, foto4, foto5];
+const carruselFotos = [foto6, foto7, foto8, foto9, foto10];
 
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
